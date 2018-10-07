7eb94b294fa257e232d5eead11eee5c4cd4c1278
pausanchezv <pausanchez.admifin@gmail.com>
2018-10-07 11:48:07 +0200

    2x1 apps

--- DjangoWebsites/settings.py
+++ DjangoWebsites/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test2.apps.Test2Config',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- DjangoWebsites/urls.py
+++ DjangoWebsites/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^test2/', include('test2.urls')),
     url(r'^admin/', admin.site.urls),
 ]
